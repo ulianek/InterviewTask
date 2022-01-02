1499e6aa4e32bc8fced08ca5f1f8b30e688a7453
Kamil Ulianowski <user@MacBook-Pro-user.local>
2022-01-02 03:21:16 +0100

    Task solved

--- cypress/integration/ticket-form.spec.ts
+++ cypress/integration/ticket-form.spec.ts
@@ -1,1 +1,37 @@
-// Your tests go in here. Happy coding! 🤓
+const NAME = 'testName'
+const EMAIL = 'testEmail@aa.com'
+const SUBJECT = 'testSubject'
+const MESSAGE = 'testMsg'
+
+const EXPECTED_SUCCESS_MSG = 'Thank you!'
+const EXPECTED_FAIL_MSG = 'Error!'
+
+describe('Autentykacja', () => {
+    beforeEach(() => {
+        cy.visit('')
+    })
+
+    it('Successful submission', function () {
+        cy.intercept(`${Cypress.env('apiUrl')}/v2/tickets/new`, {
+            fixture: 'validResponse.json',
+            statusCode: 200,
+        })
+        cy.fillTicketForm(NAME, EMAIL, SUBJECT, MESSAGE)
+
+        cy.get('#root h1.success')
+            .should('be.visible')
+            .should('have.text', EXPECTED_SUCCESS_MSG)
+    })
+
+    it('Unsuccessful submission', function () {
+        cy.intercept(`${Cypress.env('apiUrl')}/v2/tickets/new`, {
+            fixture: 'invalidResponse.json',
+            statusCode: 500,
+        })
+        cy.fillTicketForm(NAME, EMAIL, SUBJECT, MESSAGE)
+
+        cy.get('.fail')
+            .should('be.visible')
+            .should('have.text', EXPECTED_FAIL_MSG)
+    })
+})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,1 +1,9 @@
 import '@testing-library/cypress/add-commands'
+
+Cypress.Commands.add('fillTicketForm', (name, email, subject, msg) => {
+    cy.get('#name').type(name)
+    cy.get('#email').type(email)
+    cy.get('#subject').type(subject)
+    cy.get('#message').type(msg)
+    cy.get("button[type='submit']").click()
+})
